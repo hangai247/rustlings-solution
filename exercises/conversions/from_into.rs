bccd66f1a9336d308aacd0b22da00d8b7ad186d9
Soninkhangai Bilegdemberel <soninkhangai.bilegdemberel@asurion.com>
2024-01-09 12:40:11 +0900

    all rustlings solution

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,34 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let strings: Vec<&str> = s.split(",").collect();
+            let name = strings.get(0);
+            let age = strings.get(1);
+            match name {
+                Some(name) => {
+                    if name.len() == 0 {
+                        Person::default()
+                    } else {
+                        match age {
+                            Some(age) => match age.parse::<usize>() {
+                                Ok(age) => Person {
+                                    name: name.to_string(),
+                                    age,
+                                },
+                                Err(_) => Person::default(),
+                            },
+                            None => Person::default(),
+                        }
+                    }
+                }
+                None => Person::default(),
+            }
+        }
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,22 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+fn parse_to_color_integer(number: i16) -> Result<u8, IntoColorError> {
+    u8::try_from(number).map_err(|_| IntoColorError::IntConversion)
+}
+
+fn parse_to_color(r: i16, g: i16, b: i16) -> Result<Color, IntoColorError> {
+    let red = parse_to_color_integer(r)?;
+    let green = parse_to_color_integer(g)?;
+    let blue = parse_to_color_integer(b)?;
+    Ok(Color { red, green, blue })
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        parse_to_color(tuple.0, tuple.1, tuple.2)
     }
 }
 
@@ -48,6 +58,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        parse_to_color(arr[0], arr[1], arr[2])
     }
 }
 
@@ -55,6 +66,11 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() == 3 {
+            parse_to_color(slice[0], slice[1], slice[2])
+        } else {
+            Err(IntoColorError::BadLen)
+        }
     }
 }
 
